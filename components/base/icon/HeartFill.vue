fa8c8e77b51337118a05416bda7430f4a023f6cf
binhbdn <binh.buidn@gmail.com>
2021-10-11 16:32:12 +0700

    feat: migrate BaseIcons from flower-shop project

---
+++ components/base/icon/HeartFill.vue
@@ -0,0 +1,19 @@
+<template>
+  <IconSvgTemplate :icon-size="size">
+    <path
+      fill-rule="evenodd"
+      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+    />
+  </IconSvgTemplate>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: [String, Number],
+      default: 18,
+    },
+  },
+};
+</script>
---
+++ components/base/icon/Location.vue
@@ -0,0 +1,18 @@
+<template>
+  <IconSvgTemplate :icon-size="size">
+    <path
+      d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
+    />
+  </IconSvgTemplate>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: [String, Number],
+      default: 18,
+    },
+  },
+};
+</script>
