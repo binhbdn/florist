65aaed188e96c39d1b59184a5f252df44ed0c5e8
binhbdn <binh.buidn@gmail.com>
2021-10-11 11:48:02 +0700

    build(stylelint): put stylelint-config-rational-order before stylelint-config-prettie

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-prettier',
     "stylelint-config-rational-order",
+    'stylelint-config-prettier',
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
