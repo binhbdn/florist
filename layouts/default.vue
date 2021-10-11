b1c9e869583a320f5b125531147d2ed8e6b0556c
binhbdn <binh.buidn@gmail.com>
2021-10-11 15:49:52 +0700

    feat(script): add bootstrap.bundle.min.js before </body> with defer attribute

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,3 +6,13 @@
     <TheFooter />
   </div>
 </template>
+
+<script>
+export default {
+  head: {
+    script: [
+      { src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', defer: true , body: true },
+    ]
+  }
+}
+</script>
