5239996906cdc9888250c7b8842502aa08b646b5
binhbdn <binh.buidn@gmail.com>
2021-10-11 11:49:56 +0700

    build(stylelint): disable rule no-descending-specificity

--- stylelint.config.js
+++ stylelint.config.js
@@ -6,5 +6,7 @@ module.exports = {
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  rules: {
+    'no-descending-specificity': null,
+  }
 }
