ec280e174bc64264a9ca37f4c7a10ef6cf59ea0b
binhbdn <binh.buidn@gmail.com>
2021-10-13 00:50:35 +0700

    feat(page): :sparkles: initial about page with custom paths for each locale

---
+++ pages/gioi-thieu.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+  </main>
+</template>
+
+<script>
+export default {
+  nuxtI18n: {
+    paths: {
+      vn: '/gioi-thieu',
+      en: '/about-us',
+      ja: '/about-us',
+    },
+  },
+}
+</script>
