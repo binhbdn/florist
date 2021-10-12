f8e1088c0ede6a05932ffe7849c7750d10acea3f
binhbdn <binh.buidn@gmail.com>
2021-10-13 00:51:47 +0700

    feat(page): :sparkles: initial product page with custom paths for each locale

---
+++ pages/san-pham.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+  </main>
+</template>
+
+<script>
+export default {
+  nuxtI18n: {
+    paths: {
+      vn: '/san-pham',
+      en: '/product',
+      ja: '/product',
+    },
+  },
+}
+</script>
