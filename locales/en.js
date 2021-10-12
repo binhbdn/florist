d42e4c3999f9c96b5a3bd3d503022cafa1b06b38
binhbdn <binh.buidn@gmail.com>
2021-10-12 21:54:44 +0700

    feat: :sparkles: languages translations for navbar

--- locales/en.js
+++ locales/en.js
@@ -1,5 +1,23 @@
 export default {
   "nav": {
     "home": "Home",
-  }
+    "about": "About",
+    "service": "Services",
+    "product": "Product",
+    "showAllProducts": "Show all products",
+    "orderProducts": "Order products",
+    "cart": "Cart",
+    "checkout": "Checkout",
+    "wishList": "Wishlist",
+    "searchProduct": "Search product",
+    "productDecoCorner": "Product deco corner",
+    "blog": "Blog",
+    "contact": "Contact",
+    "account": "Account",
+    "register": "Register",
+    "signIn": "Sign in",
+    "signOut": "Sign out",
+    "dashboard": "Dashboard",
+  },
+
 }
--- locales/vn.js
+++ locales/vn.js
@@ -1,5 +1,23 @@
 export default {
   "nav": {
     "home": "Trang chủ",
-  }
+    "about": "Giới thiệu",
+    "service": "Dịch vụ",
+    "product": "Sản phẩm",
+    "showAllProducts": "Tất cả sản phẩm",
+    "orderProducts": "Đặt hàng sản phẩm",
+    "cart": "Sản phẩm đã chọn",
+    "checkout": "Thanh toán",
+    "wishList": "Sản phẩm yêu thích",
+    "searchProduct": "Tìm kiếm sản phẩm",
+    "productDecoCorner": "Góc deco sản phẩm",
+    "blog": "Blog",
+    "contact": "Liên hệ",
+    "account": "Tài khoản",
+    "register": "Đăng ký",
+    "signIn": "Đăng nhập",
+    "signOut": "Đăng xuất",
+    "dashboard": "Bảng điều khiển",
+  },
+
 }
