4e3098d16833749ec440e84cdc2d7afc771b871a
binhbdn <binh.buidn@gmail.com>
2021-10-12 21:54:44 +0700

    feat: :sparkles: languages translations for navbar

--- locales/jp.js
+++ locales/jp.js
@@ -1,5 +1,23 @@
 export default {
   "nav": {
     "home": "ホーム",
-  }
+    "about": "会社概要",
+    "service": "サービス",
+    "product": "お店",
+    "showAllProducts": "すべての製品",
+    "orderProducts": "製品を注文する",
+    "cart": "厳選された商品",
+    "checkout": "チェックアウト",
+    "wishList": "ウィッシュリスト",
+    "searchProduct": "製品を検索する",
+    "productDecoCorner": "製品デココーナー",
+    "blog": "ブログ",
+    "contact": "お問い合わせ",
+    "account": "アカウント",
+    "register": "アカウント",
+    "signIn": "ログイン",
+    "signOut": "ログアウト",
+    "dashboard": "ダッシュボード",
+  },
+
 }
