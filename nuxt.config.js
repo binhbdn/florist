8aab40442a15d4d7af4008752f583d76e3c425b5
binhbdn <binh.buidn@gmail.com>
2021-10-12 22:27:13 +0700

    refactor: set <html lang = "$i18n.locale">

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,9 +8,6 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'florist',
-    htmlAttrs: {
-      lang: 'en'
-    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
