fa6bd170c2f3d80992ce574eb7e645f80e876e3f
binhbdn <binh.buidn@gmail.com>
2021-10-12 22:27:13 +0700

    refactor: set <html lang = "$i18n.locale">

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,10 @@
 export default {
   head() {
     return {
+      htmlAttrs: {
+        lang: this.$i18n.locale,
+      },
+
       script: [
         {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
