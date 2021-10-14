4061260575f9364102c61adcb029192683c36eae
binhbdn <binh.buidn@gmail.com>
2021-10-14 09:24:41 +0700

    build(tailwind config): use JIT mode, prefix tw-, set breakpoints same as bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'jit',
+  prefix: 'tw-',
+
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
