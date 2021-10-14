3031d7a0d2e4a424c4fc5e814447b59d9b2e4475
binhbdn <binh.buidn@gmail.com>
2021-10-14 12:47:20 +0700

    build(tailwind): extend 15px 30px spacings & 15px 42px fontSize & 2px 4px letterSpacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,20 @@ module.exports = {
       'lg': '992px',
       'xl': '1200px',
     },
-    extend: {},
+    extend: {
+      spacing: {
+        '3.75': '0.9375rem', // 15px
+        '7.5': '1.875rem', // 30px
+      },
+      fontSize: {
+        '15px': '0.9375rem', // 15px
+        '42px': '2.625rem', // 42px
+      },
+      letterSpacing: {
+        '2px': '2px',
+        '4px': '4px',
+      },
+    },
   },
   variants: {
     extend: {},
