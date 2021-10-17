4ed2f54b03a1ca159fefe351fba8a05084d257c7
binhbdn <binh.buidn@gmail.com>
2021-10-18 00:11:09 +0700

    feat(page services): refactor from PJ1

---
+++ components/TheServicesForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="services__tab__details__form">
+    <h4>Yêu cầu báo giá</h4>
+    <form action="#">
+      <div class="row">
+        <div class="col-md-6 col-lg-6">
+          <input type="text" class="form-control" placeholder="Tên" />
+        </div>
+        <div class="col-md-6 col-lg-6">
+          <input type="tel" class="form-control" placeholder="Số điện thoại" />
+        </div>
+        <div class="col-md-6 col-lg-6">
+          <input type="email" class="form-control" placeholder="Email" />
+        </div>
+        <div class="col-md-6 col-lg-6">
+          <select class="form-select">
+            <option disabled>Chọn loại dịch vụ…</option>
+            <option value="1" selected>Hoa theo yêu cầu</option>
+            <option value="2">Hoa cho sự kiện</option>
+            <option value="3">Chăm sóc cảnh quan</option>
+          </select>
+        </div>
+        <div class="col-lg-12 text-center">
+          <textarea
+            class="form-control"
+            placeholder="Nội dung yêu cầu báo giá"
+          ></textarea>
+          <button type="submit" class="btn-florist btn-outline-florist">
+            Gửi yêu cầu báo giá
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style lang="scss">
+.services__tab__details__form {
+  padding: 30px;
+  background: #f7f5fa;
+  box-shadow: 5px 5px 10px 0 rgb(0 0 0 / 50%);
+
+  @media only screen and (min-width: 480px) {
+    padding: 50px;
+  }
+
+  @media only screen and (min-width: 992px) {
+    margin-left: 60px;
+  }
+
+  h4 {
+    margin-bottom: 25px;
+    color: #111827;
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  form {
+    input,
+    select {
+      height: 46px;
+      margin-bottom: 30px;
+      font-size: 16px;
+    }
+
+    select {
+      color: #888;
+    }
+
+    textarea {
+      width: 100%;
+      height: 100px;
+      margin-bottom: 30px;
+      font-size: 16px;
+      resize: none;
+    }
+  }
+}
+</style>
---
+++ components/TheServicesSidebarTab.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="services__sidebar__tab">
+    <ul class="nav nav-tabs" role="tablist">
+      <li
+        v-for="(item, index) in $t('serviceItems')"
+        :key="index"
+        class="nav-item"
+        role="presentation"
+      >
+        <button
+          :id="`btn-tab-${index + 1}`"
+          :data-bs-target="`#tabs-${index + 1}`"
+          :aria-controls="`tabs-${index + 1}`"
+          :aria-selected="index == 0"
+          :class="{ active: index == 0 }"
+          class="nav-link"
+          data-bs-toggle="tab"
+          role="tab"
+          type="button"
+        >
+          <div class="services__item">
+            <div class="services__item__icon">
+              <img :src="item.imgUrl" />
+            </div>
+            <h4>{{ item.title }}</h4>
+            <p>{{ item.content }}</p>
+          </div>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+.services__sidebar__tab {
+  @media only screen and (max-width: 991px) {
+    margin-bottom: 50px;
+  }
+
+  .nav {
+    display: initial;
+  }
+
+  .nav-tabs {
+    border-bottom: none;
+  }
+
+  .nav-item:last-child .nav-link .services__item {
+    margin-bottom: 0;
+  }
+
+  .nav-link {
+    padding: 0;
+    border: none;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+
+    &.active .services__item {
+      border-color: #ec4899;
+      box-shadow: 5px 5px 10px 0 #ec4899;
+    }
+
+    .services__item {
+      margin-bottom: 75px;
+    }
+  }
+}
+</style>
